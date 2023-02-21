e2349a4d2b9c8486a2fad6e775c0ee0b762b6276
SimoneFerrini <ferrini.simone4@gmail.com>
2023-02-21 15:24:04 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
+(es: MarioRossiVerde21)
+
+-chiedi nome
+-chiedi cognome
+-chiedi colore
+-genera un numero casuale
+-restituisci la psw generata
+
+*/
+
